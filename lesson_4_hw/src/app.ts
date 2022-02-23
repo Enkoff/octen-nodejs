819403726638f13fdf6a2f428e622f71247be4e7
Enkoff <0639818413@ukr.net>
2022-02-23 13:06:17 +0200

    add types from req and res

--- lesson_4_hw/src/app.ts
+++ lesson_4_hw/src/app.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import { port } from './config';
 
 const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send();
+});
 
 app.listen(port, () => {
     console.log('Server has started!!!');
